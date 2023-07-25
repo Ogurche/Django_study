6c84e9ba92e0f2caedc46ca187a3e2a21baea92e
Vladislav Grishin <337398@edu.itmo.ru>
2023-07-25 20:28:30 +0300

    Models add

--- test_project/test_app/admin.py
+++ test_project/test_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from test_app.models import Worker
+
 # Register your models here.
+
+admin.site.register(Worker)
--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Worker(models.Model):
+    name = models.CharField(max_length=20, blank= False)
+    second_name = models.CharField(max_length=50, blank= False)
+    salary = models.IntegerField (default=0)
+
+    def __str__(self) -> str:
+        return f'{self.name} {self.second_name}'
